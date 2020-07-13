639ae1591328ab0bbe1ff060581f5929a884921c
Juliebas <67914038+Juliebas@users.noreply.github.com>
2020-07-12 23:55:56 -0300

    Update scicrop-api-test-python.py

--- scicrop-api-test-python.py
+++ scicrop-api-test-python.py
@@ -1,2 +1,12 @@
-if __name__ == '__main__':
-	pass
+import requests
+import json
+
+
+arq = open("Julio.json", 'r')
+Dic = json.load(arq)
+print(Dic)
+
+r = requests.post("https://engine.scicrop.com/scicrop-engine-web/api/v1/jobs/post_resume", json=Dic)
+print("Status code: ", r.status_code)
+print(r.json())
+~                
